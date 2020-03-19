a4f976d21d769c50e99072c7935a54b955e62fb2
Jannis R <mail@jannisr.de>
2020-03-19 18:23:54 +0100

    JSONStream + SSE -> WebSocket + ndjson

--- browser.js
+++ browser.js
@@ -1,9 +1,25 @@
 var L = require('leaflet')
-var ssejson = require('ssejson')
+const {parse} = require('ndjson')
 var geojsonStream = require('leaflet-stream')
 
+const wsUrl = new URL(location.href)
+wsUrl.protocol = 'ws'
+const ws = new WebSocket(wsUrl.href)
+ws.binaryType = 'arraybuffer'
+
 var map = require('leaflet-default')()
+const parser = parse()
+parser.pipe(geojsonStream(map))
 
-ssejson.fromEventSource('/sse')
-  .pipe(geojsonStream(map))
-
+ws.onclose = (ev) => {
+	if (ev.wasClean) {
+		parser.end()
+	} else {
+		const err = new Error(`WebSocket closed: ${ev.code} ${ev.reason}`)
+		err.code = ev.code
+		parser.destroy(err)
+	}
+}
+ws.onmessage = (ev) => {
+	parser.write(Buffer.from(ev.data))
+}
