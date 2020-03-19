c4d30953146f408c6d2960ac0a9b7bd3b9baab82
Jannis R <mail@jannisr.de>
2020-03-19 17:42:08 +0100

    CLI: support --help, --version

--- cli.js
+++ cli.js
@@ -1,17 +1,38 @@
 #!/usr/bin/env node
 
 var viewGeojson = require('./')
+const mri = require('mri')
+const pkg = require('./package.json')
 var json = require('JSONStream')
 var open = require('open')
-var args = process.argv.splice(2)
 var fs = require('fs')
+
+const args = mri(process.argv.slice(2), {
+	boolean: [
+		'help', 'h',
+		'version', 'v',
+	]
+})
+
+if (args.help || args.h) {
+	process.stdout.write(`\
+cat feature1.geojson feature2.geojson | view-geojson
+view-geojson < data.ndjson
+view-geojson feature1.geojson
+`)
+	process.exit()
+}
+if (args.version || args.v) {
+	process.stdout.write(`${pkg.name} v${pkg.version}\n`)
+	process.exit()
+}
 
 var port = 9966
 
 var input = process.stdin
 
-if(args[0]) {
-  input = fs.createReadStream(args[0])
+if (args._[0]) {
+  input = fs.createReadStream(args._[0])
 }
 
 input
